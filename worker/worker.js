cbf311b165ff7ff3179c6fb078d2a5bb0aeb2071
Roy Hashimoto <roy@shoestringresearch.com>
2020-02-18 19:49:54 +0000

    Catch exceptions in the worker.

--- worker/worker.js
+++ worker/worker.js
@@ -3,20 +3,27 @@
 (() => {
   let f = undefined;
   onmessage = async (event) => {
-    switch (event.data.request) {
-    case 'create':
-      f = new Function('args', event.data.code);
+    try {
+      switch (event.data.request) {
+      case 'create':
+        f = new Function('args', event.data.code);
+        postMessage({
+          requestId: event.data.requestId,
+          functionId: event.data.requestId
+        });
+        break;
+      case 'call':
+        postMessage({
+          requestId: event.data.requestId,
+          result: await f.call(f, event.data.args)
+        });
+        break;
+      }
+    } catch (e) {
       postMessage({
         requestId: event.data.requestId,
-        functionId: event.data.requestId
+        error: e
       });
-      break;
-    case 'call':
-      postMessage({
-        requestId: event.data.requestId,
-        result: await f.call(f, event.data.args)
-      });
-      break;
     }
   };
 })();
