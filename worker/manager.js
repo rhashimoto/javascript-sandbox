b49c5b52c7ad8dc5e78031265681aeebe8bce641
Roy Hashimoto <roy@shoestringresearch.com>
2020-02-18 15:48:54 +0000

    Handle requests in a worker.

--- worker/manager.js
+++ worker/manager.js
@@ -1,38 +1,38 @@
 'use strict';
 
-// Install the service worker.
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+(() => {
+  let serviceWorkerRegistration = new Promise((resolve, reject) => {
+    // Install the service worker.
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('./sw.js').then(resolve, reject);
+      });
+    } else {
+      reject(new Error('ServiceWorker not supported in this browser.'));
+    }
+  });
+
+  let requests = new Map();
+  let worker = serviceWorkerRegistration.then(() => {
+    if ('Worker' in window) {
+      let worker = new Worker('worker.js');
+      worker.addEventListener('message', (event) => {
+        requests.get(event.data.requestId)(event);
+        requests.delete(event.data.requestId);
+      });
+      return worker;
+    }
+    throw new Error('Worker not supported in this browser.');
+  });
+
+  window.addEventListener('message', async (event) => {
+    console.log('manager message', event);
+
+    requests.set(event.data.requestId, (response) => {
+      event.source.postMessage(response.data);
     });
+    (await worker).postMessage(event.data);
   });
-} else {
-  console.log('ServiceWorker not supported in this browser.');
-}
-
-// Install the web worker.
-setTimeout(() => {
-  if ('Worker' in window) {
-    let worker = new Worker('worker.js');
-    console.log(worker);
-  } else {
-    console.log('Worker not supported in this browser.');
-  }
-}, 3000);
-
-// TODO: Handle messages.
-window.addEventListener('message', (event) => {
-  console.log('manager message', event);
-
-  event.source.postMessage({
-    requestId: event.data.requestId,
-    functionId: event.data.requestId
-  });
-});
+})();
 
 console.log('Hello, manager!');
--- worker/worker.js
+++ worker/worker.js
@@ -1,6 +1,37 @@
 'use strict';
 
-console.log('Hello, worker!');
-fetch('https://www.google.com', { mode: 'cors' }).then((response) => {
-  console.log('worker response', response);
-});
+(() => {
+  let functions = new Map();
+  onmessage = async (event) => {
+    console.log('worker message', event);
+    switch (event.data.request) {
+    case 'create':
+      functions.set(event.data.requestId, new Function('args', event.data.code));
+      postMessage({
+        requestId: event.data.requestId,
+        functionId: event.data.requestId
+      });
+      break;
+    case 'destroy':
+      functions.delete(event.data.functionId);
+      postMessage({
+        requestId: event.data.requestId
+      });
+      break;
+    case 'call':
+      let f = functions.get(event.data.functionId);
+      if (f) {
+        postMessage({
+          requestId: event.data.requestId,
+          result: await f.call(f, event.data.args)
+        });
+      } else {
+        postMessage({
+          requestId: event.data.requestId,
+          error: new Error('function not found')
+        });
+      }
+      break;
+    }
+  };
+})();
